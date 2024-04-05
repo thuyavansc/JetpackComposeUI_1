ae702aa2e746e179b72e452a7298578aac2dc3bc
Kingston <Kingston@DESKTOP-MVP19NT>
2024-04-05 22:26:05 +0530

    constraint Layout with buttons

--- app/src/main/java/au/com/softclient/myapplication/MainActivity.kt
+++ app/src/main/java/au/com/softclient/myapplication/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,7 +19,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import au.com.softclient.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,8 +34,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android");
-                    CreateUI();
+                    //Greeting("Android");
+                    //CreateUI();
+                    ConstraintView();
 
                 }
             }
@@ -64,10 +68,57 @@ fun CreateUI(){
             .fillMaxWidth()
             .fillMaxHeight()
             .background(Color.White)){
-        Column(Modifier.verticalScroll(rememberScrollState()).fillMaxWidth()) {
+        Column(
+            Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()) {
             repeat(50){ Text(text = "Hello Jaffna", fontSize = 20.sp)}
             Text(text = "Hello World!", fontSize = 16.sp)
         }
 
     }
+}
+
+@Preview
+@Composable
+fun ConstraintView(){
+    ConstraintLayout(
+        Modifier
+            .fillMaxSize()
+            .background(Color.Yellow)) {
+
+        val(one, two, three, four) = createRefs();
+
+        Button(onClick = { /*TODO*/ }, Modifier.constrainAs(one){
+            top.linkTo(parent.top, 18.dp)
+            bottom.linkTo(parent.bottom, 18.dp)
+            start.linkTo(parent.start, 18.dp)
+            end.linkTo(parent.end, 18.dp)
+        }) {
+            Text(text = "One Button");
+        }
+        Button(onClick = { /*TODO*/ }, Modifier.constrainAs(two){
+            top.linkTo(one.bottom, 18.dp)
+            start.linkTo(one.start, 18.dp)
+            end.linkTo(one.end)
+        }) {
+            Text(text = "Two Button");
+        }
+        Button(onClick = { /*TODO*/ }, Modifier.constrainAs(three){
+            top.linkTo(two.bottom, 18.dp)
+            start.linkTo(two.start, 18.dp)
+            end.linkTo(two.end)
+        }) {
+            Text(text = "Three Button");
+        }
+        Button(onClick = { /*TODO*/ }, Modifier.constrainAs(four){
+            top.linkTo(three.bottom, 18.dp)
+            start.linkTo(three.start)
+            end.linkTo(three.end)
+        }) {
+            Text(text = "Four Button");
+        }
+
+    }
+
 }
